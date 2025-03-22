485bf2fa00c61ed2d3090128f861ea95b86d7152
Shitalj90 <shitalg17@gmail.com>
2025-03-23 00:03:04 +0530

    23-March

---
+++ src/ex_01_Java_Basic/Lab008_Comments_Single_Multiple.java
@@ -0,0 +1,24 @@
+package ex_01_Java_Basic;
+
+public class Lab008_Comments_Single_Multiple {
+
+    /**
+     * Author - Pramod
+     * Course :  ATB11x
+     * This is a documentation comment
+     * @param args command-line arguments
+     * @return void
+     */
+    public static void main(String[] args) {
+        // Method implementation
+
+//        System.out.println("1");
+//        System.out.println("2");
+    }
+
+
+
+
+
+
+}
