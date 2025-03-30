80e0cecb33e330679ecb4b150e07feb77a253c44
Shitalj90 <shitalg17@gmail.com>
2025-03-30 23:05:02 +0530

    30-March

---
+++ src/ex_02_Java_Basics_part2/Lab014_Variables.java
@@ -0,0 +1,13 @@
+package ex_02_Java_Basics_part2;
+
+public class Lab014_Variables {
+    public static void main(String[] args) {
+
+    }
+
+    //we can creat multiple main function.
+    // below will not work
+    public static void main(int args) {
+
+    }
+}
